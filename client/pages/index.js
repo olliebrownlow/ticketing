addcb77fecf0997235920629f80eb81fbdc4e9ba
Oliver Brownlow <olliebrownlow@gmail.com>
2020-07-28 07:28:47 +0100

    allows requests to be made on the server to the auth-srv to get currentuser cookie

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,35 @@
-export default () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  //axios.get("/api/users/currentuser");
+
   return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === "undefined") {
+    // we are on the server
+    // requests should be made to http://SERVICENAME.NAMESPACE.svc.cluster.local
+    // this utilises the ingress-nginx-controller namespace not currently available on minikube
+    // below is the workaround
+    const { data } = await axios.get(
+      "http://auth-srv:3000/api/users/currentuser",
+      {
+        headers: req.headers,
+      }
+    );
+
+    return data;
+  } else {
+    // we are on the browser
+    // requests can be made with a base url of ""
+    const { data } = await axios.get("/api/users/currentuser");
+
+    return data;
+  }
+
+  return {};
+};
+
+export default LandingPage;
