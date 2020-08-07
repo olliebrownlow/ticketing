72758761400f9ff45386b9a4210aa86f2a5284df
Oliver Brownlow <olliebrownlow@gmail.com>
2020-08-07 07:41:02 +0100

    builds session in test setup and passes test to sign in a user

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,11 +2,12 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import { app } from "../app";
 import request from "supertest";
+import jwt from "jsonwebtoken";
 
 declare global {
   namespace NodeJS {
     interface Global {
-      signin(): Promise<string[]>;
+      signin(): string[];
     }
   }
 }
@@ -39,11 +40,25 @@ afterAll(async () => {
 
 // Should not reach out to the auth service so need to build
 // a session to mimic authentication for tests
-global.signin = async () => {
+global.signin = () => {
   // Build a JWT payload. { id,email }
+  const payload = {
+    id: "oiwejdpijp",
+    email: "test@test.com",
+  };
+
   // Create the JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
   // Build session object { jwt:MY_JWT }
+  const session = { jwt: token };
+
   // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
   // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
   // Return a string that's the cookie with the encoded data
+  return [`express:sess=${base64}`];
 };
