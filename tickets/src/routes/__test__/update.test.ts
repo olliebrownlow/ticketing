617b86ed9b6847717f2428e42d9541015bc09b65
Oliver Brownlow <olliebrownlow@gmail.com>
2020-08-09 23:44:22 +0100

    adds 2 tests for checking user authentication and a non-existent ticket id

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,9 +1,29 @@
 import request from "supertest";
 import { app } from "../../app";
+import mongoose from "mongoose";
 
-it("returns a 404 if the provided id does not exist", async () => {});
+it("returns a 404 if the provided id does not exist", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .set("Cookie", global.signin())
+    .send({
+      title: "aslkdfj",
+      price: 20,
+    })
+    .expect(404);
+});
 
-it("returns a 401 if the user is not authenticated", async () => {});
+it("returns a 401 if the user is not authenticated", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .send({
+      title: "aslkdfj",
+      price: 20,
+    })
+    .expect(401);
+});
 
 it("returns a 401 if the user does not own the ticket", async () => {});
 
